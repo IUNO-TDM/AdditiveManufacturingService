c5fd5cab820791b7439412c135b2d057ef2e222e
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-07-11 17:15:20 +0200

    added websocket client and server

---
+++ AdditiveManufacturingService-app/websocket/license_client.js
@@ -0,0 +1,115 @@
+/**
+ * Created by goergch on 09.03.17.
+ *
+ * This is a websocket client that registers on rooms on the marketplace core.
+ *
+ */
+
+const EventEmitter = require('events').EventEmitter;
+const util = require('util');
+const logger = require('../global/logger');
+const io = require('socket.io-client');
+const config = require('../config/config_loader');
+const authServer = require('../adapter/auth_service_adapter');
+
+const LicenseClient = function () {
+    const self = this;
+    this.registeredRooms = {};
+    this.refreshTokenAndReconnect = function () {
+        authServer.invalidateToken();
+        authServer.getClientAccessToken(function (err, token) {
+            if (err) {
+                logger.warn(err);
+            }
+            if (self.socket) {
+                self.socket.io.opts.extraHeaders = {
+                    Authorization: 'Bearer ' + (token ? token.accessToken : '')
+                };
+                self.socket.io.disconnect();
+                self.socket.connect();
+            }
+        });
+    };
+};
+
+const license_client = new LicenseClient();
+util.inherits(LicenseClient, EventEmitter);
+
+license_client.socket = io.connect('http://' + config.HOST_SETTINGS.MARKETPLACE_CORE.HOST
+    + ":" + config.HOST_SETTINGS.MARKETPLACE_CORE.PORT + "/licenses", {
+    transports: ['websocket'],
+    extraHeaders: {},
+    autoConnect: false
+});
+
+license_client.socket.on('connect', function () {
+    logger.info("[license_service] connected to license SocketIO at Marketplace");
+
+    // register rooms on reconnect
+    Object.keys(license_client.registeredRooms).forEach(function (room) {
+        if (!room) {
+            return;
+        }
+        logger.debug('[license_service] Re-Joining room: ' + room);
+        license_client.socket.emit('room', room);
+    });
+});
+
+license_client.socket.on('error', function (error) {
+    logger.debug("[license_service] Error: " + error);
+
+    license_client.refreshTokenAndReconnect();
+});
+
+license_client.socket.on('connect_failed', function (error) {
+    logger.debug("[license_service] Connection Failed: " + error);
+});
+
+license_client.socket.on('connect_error', function (error) {
+    logger.warn("[license_service] Connection Error: " + error);
+});
+
+license_client.socket.on('reconnect_error', function (error) {
+    logger.debug("[license_service] Re-Connection Error: " + error);
+});
+
+license_client.socket.on('reconnect_attempt', function (number) {
+    logger.debug("[license_service] Re-Connection attempt: " + number);
+});
+
+license_client.socket.on('disconnect', function () {
+    logger.info("[license_service] disconnected from license SocketIO at Marketplace");
+});
+
+/**
+ * updateAvailable events are directly passed to the registered clients. Via the license service.
+ */
+license_client.socket.on('updateAvailable', function (data) {
+    license_client.emit('updateAvailable', data.offerId, data.hsmId);
+});
+
+
+license_client.registerUpdates = function (hsmId) {
+    /**
+     * The subscriptions for license updates are separated by the hsmId.
+     */
+    if (!hsmId) {
+        return;
+    }
+
+    license_client.registeredRooms[hsmId] = true;
+
+    license_client.socket.emit('room', hsmId);
+};
+
+license_client.unregisterUpdates = function (hsmId) {
+    if (license_client.registeredRooms[hsmId]) {
+        delete  license_client.registeredRooms[hsmId];
+    }
+
+    license_client.socket.emit('leave', hsmId);
+};
+
+license_client.socket.open();
+
+module.exports = license_client;
---
+++ AdditiveManufacturingService-app/websocket/socket_io_controller.js
@@ -0,0 +1,67 @@
+/**
+ * Created by beuttlerma on 08.02.17.
+ *
+ * Socket IO Server that provides a WS interface to subscribe on license updates.
+ *
+ * The client can a join a room, where the hsmId (cmdongleid) is used as room identifier.
+ * After joining a certain room, the client gets a 'updateAvailable' event each time a new license update is available
+ * for the subscribed hsmId.
+ */
+
+const logger = require('../global/logger');
+const authentication = require('../services/authentication_service');
+const licenseClient = require('./license_client');
+// const protocol_service = require('../services/protocol_service');
+
+
+function onIOLicenseConnect(socket) {
+    logger.debug('Client for License Updates connected.' + socket.id);
+
+    const protocol = {
+        eventType: 'connection',
+        timestamp: new Date().toISOString(),
+        payload: {
+            connected: true
+        }
+    };
+    // protocol_service.createProtocolForClientId(socket.token.client.id, protocol);
+
+
+    socket.on('room', function (hsmId) {
+        logger.debug('Client joining license room: ' + hsmId);
+        socket.join(hsmId);
+        licenseClient.registerUpdates(hsmId)
+    });
+    socket.on('leave', function (hsmId) {
+        logger.debug('Client joining license room: ' + hsmId);
+        socket.leave(hsmId);
+        licenseClient.unregisterUpdates(hsmId)
+    });
+    socket.on('disconnect', function () {
+        logger.debug('Socket disconnected: ' + socket.id);
+        const protocol = {
+            eventType: 'connection',
+            timestamp: new Date().toISOString(),
+            payload: {
+                connected: false
+            }
+        };
+        // protocol_service.createProtocolForClientId(socket.token.client.id, protocol);
+    });
+}
+
+module.exports = function (io) {
+    const namespace = io.of('/licenses');
+
+    // Enable bearer token security for websocket server
+    namespace.use(authentication.ws_oAuth);
+    namespace.on('connection', onIOLicenseConnect);
+    registerLicenseEvents(namespace);
+
+};
+
+function registerLicenseEvents(namespace) {
+    licenseClient.on('updateAvailable', function (offerId, hsmId) {
+        namespace.to(hsmId).emit('updateAvailable', {hsmId: hsmId, offerId: offerId});
+    })
+}
